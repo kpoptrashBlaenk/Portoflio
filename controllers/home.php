253f9ea110bb615c73aed411fde48fa4517c113c
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-07-26 10:59:46 +0200

    Added Recommendation

--- controllers/home.php
+++ controllers/home.php
@@ -9,6 +9,7 @@ require basePath('views/partials/head.php');
 require basePath('views/partials/navbar.php');
 require basePath('views/home/jumbotron.view.php');
 require basePath('views/home/languages.view.php');
+require basePath('views/home/recommendation.view.php');
 require basePath('views/partials/tiles.view.php');
 require basePath('views/partials/footer.php');
 require basePath('views/partials/foot.php');
