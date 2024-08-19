86c177a9c9b9e0dab1159702b420447fe80c6dde
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-08-19 12:06:26 +0200

    Added: Project Carousels

--- public/resources/js/script.js
+++ public/resources/js/script.js
@@ -5,17 +5,17 @@ $(document).ready(function () {
     let $stickyElements = $('.sticky-title');
     let $stickyBorders = $('.sticky-border');
 
-    for(let i = 0; i < $stickyElements.length; i++) {
+    for (let i = 0; i < $stickyElements.length; i++) {
         stickyElement($($stickyElements[i]), $($stickyBorders[i]));
     }
 
     $(window).on('scroll', function () {
-        for(let i = 0; i < $stickyElements.length; i++) {
+        for (let i = 0; i < $stickyElements.length; i++) {
             stickyElement($($stickyElements[i]), $($stickyBorders[i]));
         }
     });
 
-    function stickyElement (element, border) {
+    function stickyElement(element, border) {
         if ($(window).scrollTop() >= element.offset().top - 5) {
             border.removeClass('border-bottom');
         } else {
@@ -39,4 +39,47 @@ $(document).ready(function () {
     setTimeout(function () {
         $('#rainbow').addClass('d-none')
     }, 3000)
+
+
+    // PROJECT CAROUSEL
+    if (window.matchMedia("(min-width:576px)").matches) {
+        let carouselInner = $('.carousel-inner');
+        let carouselItem = $('.carousel-item');
+        let cardWidth;
+        setCardWidth();
+
+        $(window).on('resize', setCardWidth);
+
+        function setCardWidth() {
+            cardWidth = carouselItem.outerWidth(true);
+        }
+
+        function moveFirstToLast() {
+            let firstItem = carouselItem.first();
+            firstItem.remove();
+            carouselInner.append(firstItem);
+            carouselInner.scrollLeft(carouselInner.scrollLeft() - cardWidth);
+        }
+
+        function moveLastToFirst() {
+            let lastItem = carouselItem.last();
+            lastItem.remove();
+            carouselInner.prepend(lastItem);
+            carouselInner.scrollLeft(carouselInner.scrollLeft() + cardWidth);
+        }
+
+        $('.carousel-control-next').on('click', function () {
+            carouselInner.animate({scrollLeft: '+=' + cardWidth}, 600, function () {
+                moveFirstToLast();
+            });
+        });
+
+        $('.carousel-control-prev').on('click', function () {
+            moveLastToFirst();
+            carouselInner.animate({scrollLeft: '-=' + cardWidth}, 600);
+        });
+    }
+
+
+
 });
---
+++ views/projects/carousel.view.php
@@ -0,0 +1,86 @@
+<div id="carouselExample" class="carousel">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+        <div class="carousel-item">
+            <div class="card" style="width: 18rem;">
+                <div class="">
+                    <img src="resources/images/AnyaAOTXP.png" class="card-img-top" alt="...">
+                </div>
+                <div class="card-body">
+                    <h5 class="card-title">Card title</h5>
+                    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                </div>
+            </div>
+        </div>
+    </div>
+    <button class="carousel-control-prev m-0" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next m-0" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
+</div>
