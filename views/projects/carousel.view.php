aa81ed50ec9c86683bab17996a0b141f13842681
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-08-20 10:59:03 +0200

    Updated: Carousel-Controls
    Also made style.css somewhat clearer.

--- views/projects/carousel.view.php
+++ views/projects/carousel.view.php
@@ -8,13 +8,13 @@
     <div class="mt-2 border-bottom border-secondary"></div>
 
     <div id="carouselExample" class="carousel d-flex justify-content-between position-relative">
-        <button class="prev-button m-0 position-absolute translate-middle-y top-50 start-0 border-tertiary z-1" style="height:3em;padding-top:0.4em" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
+        <button class="prev-button m-0 position-absolute translate-middle-y top-50 start-0 border-tertiary z-1 rounded" style="height:3em;padding-top:0.4em" type="button" data-bs-target="#carouselExample" data-bs-slide="prev">
             <span class="carousel-control-prev-icon m-0" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
         <div id="carouselInner" class="carousel-inner">
         </div>
-        <button class="next-button m-0 position-absolute translate-middle-y top-50 end-0 border-tertiary z-1" style="height:3em;padding-top:0.4em" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
+        <button class="next-button m-0 position-absolute translate-middle-y top-50 end-0 border-tertiary z-1 rounded" style="height:3em;padding-top:0.4em" type="button" data-bs-target="#carouselExample" data-bs-slide="next">
             <span class="carousel-control-next-icon m-0" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
