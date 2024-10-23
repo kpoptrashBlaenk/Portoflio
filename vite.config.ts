cfb61cc612c166569aa9370d73debff321833eb2
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-23 12:27:10 +0200

    Updated for render server

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,15 @@
 import { defineConfig } from "vite"
 import vue from "@vitejs/plugin-vue"
 
+console.log("VITE_HOST:", process.env.VITE_HOST)
+console.log("VITE_PORT:", process.env.VITE_PORT)
+
 export default defineConfig({
   plugins: [vue()],
   base: "/Portfolio-Custom",
   server: {
-    host: "0.0.0.0",
+    host: process.env.VITE_HOST || "localhost",
     // @ts-ignore
-    port: process.env.PORT || 3000,
+    port: process.env.VITE_PORT || 3000,
   },
 })
