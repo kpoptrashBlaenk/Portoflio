433945ff005edb5d04938c250447ef7f82e91f01
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-23 11:56:17 +0200

    Updated for render server

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from "vite"
+import vue from "@vitejs/plugin-vue"
 
 export default defineConfig({
   plugins: [vue()],
-  base: '/Portfolio-Custom',
+  base: "/Portfolio-Custom",
+  server: {
+    host: "0.0.0.0",
+    // @ts-ignore
+    port: process.env.PORT || 3000,
+  },
 })
