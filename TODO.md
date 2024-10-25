b33015de7afbdf9d50fdc8ec9c51676e50b9e37a
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-10-25 14:22:57 +0200

    Add: Legal Mentions in footer

--- TODO.md
+++ TODO.md
@@ -1,2 +1,1 @@
-// TODO: Make Legal Mentions
 // TODO: Make Mobile
--- src/components/partials/Footer.vue
+++ src/components/partials/Footer.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container">
-    <footer class="d-flex flex-wrap justify-content-between align-items-center py-3 my-4 border-top">
-      <div class="col-md-4 d-flex align-items-center">
+    <footer class="d-flex flex-wrap justify-content-between py-3 my-4 border-top">
+      <div class="col-md-1 d-flex">
         <a href="https://github.com/kpoptrashBlaenk"
           class="mb-3 me-2 mb-md-0 text-body-secondary text-decoration-none lh-1">
           <i class="fab fa-linkedin"></i>
@@ -11,7 +11,24 @@
         </a>
       </div>
 
-      <ul class="nav col-md-4 justify-content-end list-styled d-flex">
+      <div class="col-md-8 d-flex justify-content-between">
+        <div class="d-flex flex-column justify-content-start px-5 fw-bold">
+          <h2>Owner</h2>
+          <div>Aldin Music</div>
+          <div>aldinmusik@gmail.com</div>
+          <div>Hosted by ©Render</div>
+          <div>Portfolio</div>
+        </div>
+        <div class="d-flex flex-column justify-content-start px-5">
+          <h2>Legal Mentions</h2>
+          <div class="mb-2">All content, including text, images, and files, on this website is the property of Aldin Music and is
+            protected by intellectual property laws.</div>
+          <div class="mb-2">This website does not collect any personal data from its users.</div>
+          <div class="mb-2">These legal mentions are governed by the laws of France.</div>
+        </div>
+      </div>
+
+      <ul class="nav col-md-1 justify-content-end list-styled d-flex">
         <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fab fa-tiktok"></i></a></li>
         <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fab fa-instagram"></i></a></li>
         <li class="ms-3"><a class="text-body-secondary" href="#"><i class="fab fa-reddit"></i></a></li>
