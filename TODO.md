ce89475232c49799fdd352b0c96aba7051e0fc95
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-11-18 09:52:50 +0100

    Recommendation: Screw that

--- TODO.md
+++ TODO.md
@@ -1,3 +1,2 @@
-// TODO: Recommendations Title is too big also make recommendations become quotes of famous people saying good stuff about me
 // TODO: Make Accordion work on scroll as well
 // TODO: Remove Jquery
--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,20 +1,17 @@
 <template>
   <Jumbotron />
   <Languages />
-  <Recommendation />
 </template>
 
 <script setup lang="ts">
 import Jumbotron from "../components/home/Jumbotron.vue"
 import Languages from "../components/home/Languages.vue"
-import Recommendation from "../components/home/Recommendation.vue"
 
 defineOptions({
   name: 'Home',
   components: {
     Jumbotron,
-    Languages,
-    Recommendation
+    Languages
   }
 })
 </script>
