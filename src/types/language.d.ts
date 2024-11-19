2dc60148d3a62985f365b86709a3e3302b0f7480
kpoptrashBlaenk <aldinmusik@gmail.com>
2024-11-19 22:47:17 +0100

    Language: Init

---
+++ src/types/language.d.ts
@@ -0,0 +1,13 @@
+export type Language = {
+  english: string
+  german: string
+  french: string
+}
+
+export type NavigationLanguage = {
+  home: Language
+  parcours: Language
+  projects: Language
+  knowledge: Language
+  bts: Language
+}
